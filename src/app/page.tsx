cb01e36da9ce4ef91809d362dacb425601ae4821
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-02-01 23:20:13 +0900

    Chore: tailwind theme 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Init</div>;
+  return <div className=" bg-cta">Init</div>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+      },
+      colors: {
+        background: "rgba(var(--background))",
+        border: "rgba(var(--border))",
+        card: "rgba(var(--card))",
+        "copy-primary": "rgba(var(--copy-primary))",
+        "copy-secondary": "rgba(var(--copy-secondary))",
+        cta: "rgba(var(--cta))",
+        "cta-active": "rgba(var(--cta-active))",
+        "cta-text": "rgba(var(--cta-text))",
       },
     },
   },
