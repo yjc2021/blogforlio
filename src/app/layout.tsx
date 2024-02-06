171fa0426dbfbfc5736a084281e9e5777860e9a4
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-02-06 23:50:40 +0900

    Feat: 기본 페이지 구조 구성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
+import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"], display: "swap", variable: "--font-inter" });
 
@@ -18,7 +20,12 @@ export default function RootLayout({
   return (
     <html lang="kr" className={`${inter.className}`}>
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <Navigation />
+          <div className="h-full">{children}</div>
+
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,5 @@
-"use client";
-
-import PaletteCard from "@/components/PaletteCard";
-import ToggleSwitch from "@/components/ToggleSwitch";
-
-const Home = () => {
-  return (
-    <>
-      <ToggleSwitch />
-      <div className="flex-col gap-2">
-        <PaletteCard backgroundColor="bg-cta" />
-        <PaletteCard backgroundColor="bg-cta-active" />
-        <PaletteCard backgroundColor="bg-cta-text" />
-        <PaletteCard backgroundColor="bg-card" />
-        <PaletteCard backgroundColor="bg-background" />
-        <PaletteCard backgroundColor="bg-border" />
-      </div>
-    </>
-  );
+const LandingPage = () => {
+  return <div>Landing</div>;
 };
 
-export default Home;
+export default LandingPage;
